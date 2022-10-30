316c69839196afa7c4f05e6aac24d22ea10c2171
code-it-samurai <prathameshtakane76@gmail.com>
2022-10-30 22:43:54 +0530

    added: greeting and try-catch for every options function

--- modules.js
+++ modules.js
@@ -1,37 +1,70 @@
+const chalk = require("chalk");
+
 require("dotenv").config({ path: __dirname + "/.env" });
 const envDataParser = require("./helpers/commands").envDataParser;
 const collectDataTypes = require("./helpers/commands").collectDataTypes;
 let optionsDataTypes = {};
+const greetings = process.env.GREETING;
 
 module.exports = {
 	questions: [
 		{
 			name: "TASK_TYPE",
 			type: "list",
-			message: "what do you want to do?",
+			message: `${greetings} what are you planning to work with?`,
 			choices: () => {
-				let options = envDataParser(process.env.InitialCommands, 'object[]');
-				optionsDataTypes = {...optionsDataTypes, ...collectDataTypes(options)}; // collect data types of all the initial options
-				return options;
+				try {
+					let options = envDataParser(
+						process.env.InitialCommands,
+						"object[]"
+					);
+					optionsDataTypes = {
+						...optionsDataTypes,
+						...collectDataTypes(options),
+					}; // collect data types of all the initial options
+					return options;
+				} catch(error) {
+					console.log(chalk.red("Apologies sir! There seems to be an issue with my code, here are the details"));
+					console.error(chalk.red(`${error}`));
+					return []
+				}
+
 			},
 		},
 		{
 			name: "TASK_TARGET",
 			type: "list",
-			message: "project?",
+			message:
+				"Ahh i see! Please select a command suitable for your required task",
 			choices: function (previousAnswer) {
-				let options = envDataParser(process.env[previousAnswer.TASK_TYPE], optionsDataTypes[previousAnswer.TASK_TYPE]);
-				return options;
-			},
+				try {
+					let options = envDataParser(
+						process.env[previousAnswer.TASK_TYPE],
+						optionsDataTypes[previousAnswer.TASK_TYPE]
+					);
+					return options;
+				} catch(error) {
+					console.log(chalk.red("Apologies sir! There seems to be an issue with my code, here are the details"));
+					console.error(chalk.red(`${error}`));
+					return []
+				}
+			}
+			,
 		},
 		{
 			name: "ACTION",
 			type: "list",
-			message: "Please choose a command from the list",
+			message:
+				"Ahh i see! Please select a command suitable for your required task",
 			choices: function (previousAnswer) {
-				console.log("HAHAHAHAA", process.env[previousAnswer.TASK_TARGET].split(","))
-				return process.env[previousAnswer.TASK_TARGET].split(",");
-			}
+				try {
+					return process.env[previousAnswer.TASK_TARGET].split(",");
+				} catch(error) {
+					console.log(chalk.red("Apologies sir! There seems to be an issue with my code, here are the details"));
+					console.error(chalk.red(`${error}`));
+					return []
+				}
+			},
 		},
 	],
 };
